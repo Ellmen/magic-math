6c1dcb2c9bd639ac4a1717f61ed3b872c450b1c4
Isaac Ellmen <isaacellmen@gmail.com>
2018-08-31 19:10:31 -0400

    index.py is a playground

--- index.py
+++
@@ -1,9 +0,0 @@
-import sys  
-sys.path.append('cards')
-sys.path.append('util')
-
-# For the time being, this file is meant to be a workspace
-# From here, you can import cards like:
-
-# from grisly_salvage import grisly_salvage
-# print grisly_salvage()
