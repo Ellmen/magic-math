281c565bdb1499dbb2da0c5377c5e8377563219e
Isaac Ellmen <isaacellmen@gmail.com>
2018-08-31 18:21:10 -0400

    Grisly salvage

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # magic-math
-Math and code for misc MTG probabilities
+Math and code for misc MTG probabilities. Above all else, the code in this repository is intended to be easy to read and easy to use.
---
+++ grisly.py
@@ -0,0 +1,23 @@
+# Calculates how frequently grisly salvage will hit a certain
+# card, given the number of cards left in the deck
+
+from random import shuffle
+
+good_cards = ['good'] * 4
+bad_cards = ['bad'] * 56
+
+num_cards_played = 9
+
+played = bad_cards[:num_cards_played]
+deck = good_cards + bad_cards[num_cards_played:]
+
+hits = 0
+
+trials = 10 ** 4
+
+for _ in range(trials):
+   shuffle(deck)
+   if 'good' in deck[:5]:
+       hits += 1
+
+print hits / float(trials)
